18d82e84b56af92e332abfab30af6b7e60ae2a59
PatelShakil <patelsakib95@gmail.com>
2024-07-10 13:00:32 +0530

    API TESTING

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => "api"], (function () {
 
     Route::get("/common/test", function () {
-        return response()->json(["status" => true, "data" => "API Started"]);
+        return response()->json(["status" => true, "data" => "SmugLinks API Started"]);
     });
 
     Route::prefix('/common', function () {
