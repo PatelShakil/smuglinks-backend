55d6bbfac28e9b326855bc8b905957cf8046217f
PatelShakil <patelsakib95@gmail.com>
2024-07-12 09:43:49 +0530

    sdfsdf

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => "api"], (function () {
 
-    Route::get("/common/test", function () {
-        return response()->json(["status" => true, "data" => "SmugLinks API Started"]);
-    });
-
     Route::prefix('/common', function () {
-        Route::get('/check-user-exists/{username}',[UserController::class,'checkUserExists']);
+        Route::get('/test', function () {
+            return response()->json(["status" => true, "data" => "SmugLinks API Started"]);
+        });
+
+        Route::get('/check-user-exists/{username}', [UserController::class, 'checkUserExists']);
     });
 
     Route::prefix('/user', function () {
