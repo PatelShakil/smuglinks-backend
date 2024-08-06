ab51a1f61ed854e5420096429b819acdfd22844b
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-08-06 13:06:26 +0000

    Method Changes

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::group(['middleware' => "api"], (function () {
 
     Route::prefix('/user')->group(function () {
         Route::post('/login',[AuthController::class,'doLogin']);
-        Route::post('/update-yourself',[AuthController::class,'update']);
+        Route::post('/update-yourself',[UserController::class,'update']);
     });
 }));
