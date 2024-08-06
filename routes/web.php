465db6f10e262abdffe9bc047ead79fdb9de9940
M.Shakil Patel <patelshakil10@gmail.com>
2024-08-06 11:35:13 +0000

    API DOCS

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api-docs',function(){
+    $txtFile = file_get_contents("https://api.smuglinks.com/api-docs.txt");
+    echo $txtFile;
+});
