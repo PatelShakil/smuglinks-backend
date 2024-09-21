95be308212ea76757afef4cac1a3392e4bf4e550
PatelShakil <patelsakib95@gmail.com>
2024-09-21 22:51:00 +0530

    Just changed

--- routes/api.php
+++ routes/api.php
@@ -10,20 +10,20 @@ Route::group(['middleware' => "api"], (function () {
 
     Route::prefix('/common')->group(function () {
         Route::get('/test', function () {
-            return response()->json(["status" => true, "data" => null,"message"=> "SmugLinks API Started"]);
+            return response()->json(["status" => true, "data" => null, "message" => "SmugLinks API Started"]);
         });
-        Route::post('/signup',[AuthController::class,'doSignup']);
-        Route::post('/login',[AuthController::class,'doLogin']);
-        Route::get('/check-user-exists/{username}', [UserController::class, 'checkUserExists']);
+        Route::post('/signup', [AuthController::class, 'doSignup']);
+        Route::post('/login', [AuthController::class, 'doLogin']);
+        Route::get('/check-user-exists/{username}', [AuthController::class, 'checkUserExists']);
     });
 
     Route::prefix('/user')->group(function () {
-        Route::post('/update-yourself',[UserController::class,'update']);
-        Route::get('/details',[UserController::class, 'getUserDetails']);
-        Route::post('/reset-password',[UserController::class, 'resetPassword']);
+        Route::post('/update-yourself', [UserController::class, 'update']);
+        Route::get('/details', [UserController::class, 'getUserDetails']);
+        Route::post('/reset-password', [UserController::class, 'resetPassword']);
     });
-    Route::prefix('link')->group(function (){
-        Route::post('/add',[LinkController::class,'addLink']);
-        Route::get('/get-all',[LinkController::class,'getAllLinks']);
+    Route::prefix('link')->group(function () {
+        Route::post('/add', [LinkController::class, 'addLink']);
+        Route::get('/get-all', [LinkController::class, 'getAllLinks']);
     });
 }));
