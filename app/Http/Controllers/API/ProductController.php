3193c97441aaf5a5c39358ba7ddf7c8b69bf22f6
MuhammadShakil <patelsakib95@gmail.com>
2024-12-17 18:56:28 +0530

    sd

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -278,7 +278,7 @@ class ProductController extends Controller
         ]);
 
         if ($validator->fails()) {
-            print($request->all());
+            print_r($request->all());
             return response()->json([
                 "status" => false,
                 "message" => $validator->errors()->first()
