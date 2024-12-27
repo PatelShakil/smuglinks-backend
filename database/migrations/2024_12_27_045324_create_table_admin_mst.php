0688852a9d80184cea779a1eff1a84e6dcd17d60
MuhammadShakil <patelsakib95@gmail.com>
2024-12-27 10:42:17 +0530

    sadf

--- database/migrations/2024_12_27_045324_create_table_admin_mst.php
+++ database/migrations/2024_12_27_045324_create_table_admin_mst.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_admin_mst');
+        Schema::dropIfExists('admin_mst');
     }
 };
